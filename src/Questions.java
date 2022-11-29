dcd8c33829c47953f86f4f3d54eafccd33ea4dea
Sabyr1 <118902488+Sabyr1@users.noreply.github.com>
2022-11-29 17:43:52 +0600

    Update Questions.java

--- src/Questions.java
+++ src/Questions.java
@@ -1,10 +1,10 @@
 public class Questions {
     public static void main(String[] args) {
-        System.out.println("Name: Meirzhan");
-        System.out.println("Surname: Anarbekuly");
-        System.out.println("Id: 220103307");
-        System.out.println("Hobby: Football");
-        System.out.println("How much do you like ICT(from 1-10)): 11");
+        System.out.println("Name: Sabyrzhan");
+        System.out.println("Surname: Ay");
+        System.out.println("Id: 2201037");
+        System.out.println("Hobby: ");
+        System.out.println("How much do you like ICT1-10)): 11");
         System.out.println("Rate this task from 1 to 10: 11");
         System.out.println("Am I the best teacher?: Of course, you are!");
     }
